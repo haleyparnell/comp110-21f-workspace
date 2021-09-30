fc7f46fe64a1d722801c556b8a5b1e7231709905
Haley Parnell <hparnell@unc.edu>
2021-09-29 23:54:13 -0400

    Working on ex4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,20 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730514769"
 
 
 # TODO: Implement your functions here.
+
+
+def main() -> bool:
+    """Test function calls."""
+    x: list[int] = []
+    i: int = 0
+    if all in x == i:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
