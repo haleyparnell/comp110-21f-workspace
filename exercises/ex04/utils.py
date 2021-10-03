dda97977b5ccb15ee5bed5aa2004e4d5d19309b5
Haley Parnell <hparnell@unc.edu>
2021-10-03 13:02:35 -0400

    fdd

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,5 +4,11 @@ __author__ = "730514769"
 
 
 # TODO: Implement your functions here.
-def user(list):
-    return all(i == list[0] for i in list)
+
+def all_same(list):
+    user_list: list[int] = []
+    result = all(element == user_list[0] for element in user_list)
+    if (result):
+        return True
+    else:
+        return False
