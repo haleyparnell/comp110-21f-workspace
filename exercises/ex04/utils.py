8d30d0a5982be617c97bd424e24489b2b76bd8c0
Haley Parnell <hparnell@unc.edu>
2021-10-03 12:51:26 -0400

    fsdfd

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,17 +4,5 @@ __author__ = "730514769"
 
 
 # TODO: Implement your functions here.
-
-
-def main() -> bool:
-    """Test function calls."""
-    x: list[int] = []
-    i: int = 0
-    if all in x == i:
-        return True
-    else:
-        return False
-
-
-if __name__ == "__main__":
-    main()
+def user(list):
+    return all(i == list[0] for i in list)
