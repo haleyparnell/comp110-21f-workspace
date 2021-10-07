158d36e138dacf28d2fc5b923806d381c8493f00
Haley Parnell <hparnell@unc.edu>
2021-10-06 23:37:28 -0400

    df

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -9,7 +9,7 @@ def only_evens(list_1: list[int]) -> list[int]:
     """Given a list of ints, return only a list containing the ints that were even."""
     list_2 = []
     i: int = 0
-    while len(list_1) > 0:
+    while i < len(list_1):
         if list_1[i] % 2 == 0:
             list_2.append(list_1[i])
             i += 1
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,18 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730514769"
+
+
+def test_only_evens_1() -> None:
+    assert only_evens([]) == []
+
+
+def test_only_evens_2() -> None:
+    assert only_evens([1, 2, 3, 4]) == [2, 4]
+
+
+def test_only_evens_3() -> None:
+    assert only_evens([1, 2, 3, 4, 4, 2, 6]) == [2, 4, 4, 2, 6]
