1cdcefa8f632901ebebfa1f821e4ce73d5944e44
Haley Parnell <hparnell@unc.edu>
2021-10-20 12:15:08 -0400

    progress

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,10 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730514769"
+
 
 # Define your functions below
+def invert(user: dict[str, str]) -> dict[str, str]:
+    """Invert the keys and the values."""
+    user = {v: k for k, v in user.items()}
+    print(user)
